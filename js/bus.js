1cdfad5311639a11ac4ccaf878dd2a7e44774524
sameeksharathi <rathisameeksha9@gmail.com>
2020-11-28 16:28:24 +0530

    updated

--- js/bus.js
+++ js/bus.js
@@ -13,22 +13,10 @@ $(function () {
     });
 });
 
-$(document).ready(function() {
-    // Check if element is scrolled into view
-    function isScrolledIntoView(elem) {
-      var docViewTop = $(window).scrollTop();
-      var docViewBottom = docViewTop + $(window).height();
-  
-      var elemTop = $(elem).offset().top;
-      var elemBottom = elemTop + $(elem).height();
-  
-      return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
-    }
-    $(window).scroll(function() {
-      $('.scroll-animations .animated').each(function() {
-        if (isScrolledIntoView(this) === true) {
-          $(this).addClass('fadeInLeft');
-        }
-      });
-    });
+
+AOS.init({
+    offset: 200,
+    duration: 200,
+    easing: 'ease-in-quad',
+    delay: 200,
   });
