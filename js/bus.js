fe71c8f94875696d2389cdb6fecc60b6a658e427
sameeksharathi <rathisameeksha9@gmail.com>
2020-11-28 15:01:42 +0530

    updated

--- js/bus.js
+++ js/bus.js
@@ -12,3 +12,23 @@ $(function () {
     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
     });
 });
+
+$(document).ready(function() {
+    // Check if element is scrolled into view
+    function isScrolledIntoView(elem) {
+      var docViewTop = $(window).scrollTop();
+      var docViewBottom = docViewTop + $(window).height();
+  
+      var elemTop = $(elem).offset().top;
+      var elemBottom = elemTop + $(elem).height();
+  
+      return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+    }
+    $(window).scroll(function() {
+      $('.scroll-animations .animated').each(function() {
+        if (isScrolledIntoView(this) === true) {
+          $(this).addClass('fadeInLeft');
+        }
+      });
+    });
+  });
--- js/contact.js
+++ js/contact.js
@@ -1,6 +1,6 @@
-$(function () {
-    $(document).scroll(function () {
-    var $nav = $(".navbar");
-    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-    });
-});
+// $(function () {
+//     $(document).scroll(function () {
+//     var $nav = $(".navbar");
+//     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+//     });
+// });
